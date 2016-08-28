0a9aeedca4a1cc28e8ea96f2db073b5076889137
daniellasheva <daniellasheva@gmail.com>
2016-08-28 17:39:49 -0400

    new dream form

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -3,16 +3,43 @@ class DreamsController < ApplicationController
   before_action :set_dream, only: :show
 
   def new
+    @dream=Dream.new
+    @number_of_elements = 0
+  end
+
+  def add_element_form
+    byebug
+    @number_of_elements = params[]
+    render :new
   end
 
   def show
 
   end
 
+  def create
+    #byebug
+    user_id = 1
+
+    @dream = Dream.new(dream_params)
+    @dream.user_id = user_id
+
+    if @dream.save
+      redirect_to dream_path(@dream)
+    else
+      render :new
+    end
+    #create thing
+  end
+
   private
 
   def set_dream
     @dream = Dream.find(params[:id])
+  end
+
+  def dream_params
+    params.require(:dream).permit(:title, :description, :date, element_ids:[])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  post '/dreams/element_quantities', to: 'dreams#add_element_form', as: 'element_quantity_path'
+
   resources :elements
   resources :dreams
   resources :users
 
-  root "sessions#new" 
+  root "sessions#new"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
